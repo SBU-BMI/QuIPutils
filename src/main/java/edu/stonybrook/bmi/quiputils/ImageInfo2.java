220ef76da3d9f13783db89607b25157bea6e10a4
Erich Bremer <erich@ebremer.com>
2018-04-10 17:00:43 -0400

    WIP

--- src/main/java/edu/stonybrook/bmi/quiputils/ImageInfo2.java
+++ src/main/java/edu/stonybrook/bmi/quiputils/ImageInfo2.java
@@ -9,8 +9,6 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import javax.json.Json;
 import javax.json.JsonObject;
-import loci.common.services.DependencyException;
-import loci.common.services.ServiceException;
 import loci.formats.FormatException;
 import loci.formats.ImageReader;
 
@@ -19,11 +17,16 @@ import loci.formats.ImageReader;
  * @author Erich Bremer
  */
 public class ImageInfo2 {
-    public static void main(String[] args) throws DependencyException, ServiceException, FormatException, IOException, Exception {
+    public static void main(String[] args) {
         loci.common.DebugTools.setRootLevel("WARN");
         ImageReader reader = new ImageReader();
+        //reader = loci.formats.Memoizer(reader);
         String fpath = args[0];
-        reader.setId(fpath);
+        try {
+            reader.setId(fpath);
+        } catch (FormatException | IOException ex) {
+            System.out.println(ex.toString());
+        }
         JsonObject json = Json.createObjectBuilder()
             .add("field_width", Json.createArrayBuilder().add(Json.createObjectBuilder().add("value", BigDecimal.valueOf(reader.getSizeX()))))
             .add("field_height", Json.createArrayBuilder().add(Json.createObjectBuilder().add("value", BigDecimal.valueOf(reader.getSizeY()))))
